abc563a1da0a57919dce37590c99f39370d8640a
mu <masato.umeno.818@gmail.com>
2025-12-23 21:55:20 +0900

    test: setup

--- src/test/runTest.ts
+++ src/test/runTest.ts
@@ -1,16 +1,23 @@
 import * as path from "path";
-import { runTests } from "@vscode/test-electron";
+import {
+  downloadAndUnzipVSCode,
+  resolveCliPathFromVSCodeExecutablePath,
+  runTests
+} from "@vscode/test-electron";
 
 async function main(): Promise<void> {
   try {
     const extensionDevelopmentPath = path.resolve(__dirname, "../../");
     const extensionTestsPath = path.resolve(__dirname, "./suite/index");
-    const workspacePath = path.resolve(extensionDevelopmentPath, "samples");
+
+    const vscodeExecutablePath = await downloadAndUnzipVSCode("1.93.0");
+    const vscodeCliPath =
+      resolveCliPathFromVSCodeExecutablePath(vscodeExecutablePath);
 
     await runTests({
       extensionDevelopmentPath,
       extensionTestsPath,
-      launchArgs: [workspacePath]
+      vscodeExecutablePath: vscodeCliPath
     });
   } catch (error) {
     console.error("Failed to run VS Code tests.", error);
